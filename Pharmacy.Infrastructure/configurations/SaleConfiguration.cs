48f6b74ebbfe0e98d9826bfa936cc777f723c5fa
abdullahsayedsalesvision-jpg <abdullah.sayed.salesvision@gmail.com>
2026-05-12 17:24:12 +0300

    feat: add AppDbContext and entity configurations (Infrastructure Layer)

---
+++ Pharmacy.Infrastructure/configurations/SaleConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Pharmacy.Infrastructure.configurations
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.TotalAmount)
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(s => s.SaleDate)
+                .IsRequired();
+
+            builder.HasOne(s => s.User)
+                .WithMany()
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(s => s.SaleItems)
+                .WithOne(si => si.Sale)
+                .HasForeignKey(si => si.SaleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+        }
+    }
+}
---
+++ Pharmacy.Infrastructure/configurations/SaleItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Pharmacy.Infrastructure.configurations
+{
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.HasKey(si => si.Id);
+
+            builder.Property(si => si.UnitPrice)
+                   .HasColumnType("decimal(18,2)");
+
+            builder.Property(si => si.Quantity)
+                   .IsRequired();
+
+            builder.HasOne(si => si.Medicine)
+                   .WithMany()
+                   .HasForeignKey(si => si.MedicineId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
