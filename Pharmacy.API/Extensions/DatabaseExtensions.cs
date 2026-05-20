5ce5f6389499dd43c3a3beb29f8784bbfae8416c
abdullahsayedsalesvision-jpg <abdullah.sayed.salesvision@gmail.com>
2026-05-20 20:03:38 +0300

    feat(infrastructure): configure SQL Server and initial database schema

---
+++ Pharmacy.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Infrastructure;
+
+namespace Pharmacy.API.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+            return;
+
+        await using var scope = app.Services.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<PharmacyDbContext>();
+        await context.Database.MigrateAsync();
+    }
+}
--- Pharmacy.Infrastructure/DependencyInjection.cs
+++ Pharmacy.Infrastructure/DependencyInjection.cs
@@ -12,10 +12,18 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
+
         services.AddDbContext<PharmacyDbContext>(options =>
-            options.UseSqlServer(
-                configuration.GetConnectionString("DefaultConnection"),
-                sql => sql.EnableRetryOnFailure()));
+            options.UseSqlServer(connectionString, sql =>
+            {
+                sql.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+                sql.MigrationsAssembly(typeof(PharmacyDbContext).Assembly.FullName);
+            }));
 
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
